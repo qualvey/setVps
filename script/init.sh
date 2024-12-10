2a632a2fe504e410cea84bcae2cf27e28b72e715
ryu <ultralmantigar@gmail.com>
2024-12-11 00:06:11 +0800

    more update

--- script/init.sh
+++ script/init.sh
@@ -2,9 +2,11 @@
 
 #定义变量
 ports=(22 80 443 "60000:61000/udp")
-domain="kiruryu.site"
 certpath="/etc/letsencrypt/live/$domain/fullchain.pem"
 keypath="/etc/letsencrypt/live/$domain/privkey.pem"
+
+echo "Enter the domain you want to deploy or skip just enter:"
+read domain
 
 #instlal necicery
 apt install mosh docker certbot
@@ -15,8 +17,12 @@ done
 echo "指定的端口已放行：${ports[@]}"
 
 #获取ca的证书和密钥
-echo "getting CA cert "
-certbot certonly --standalone -d ${domain} -d "www.${domain}"
+if [ -z $domain ]; then
+  echo "skip acme."
+else
+  echo "getting CA cert "
+  certbot certonly --standalone -d ${domain} -d "www.${domain}"
+fi
 
 #配置trojan和hysteria
 #docker way
@@ -26,9 +32,14 @@ certbot certonly --standalone -d ${domain} -d "www.${domain}"
 #    -v /etc/trojan-go/:/etc/trojan-go \
 #    --network host \
 #    p4gefau1t/trojan-go
-mkdir trojan && cd trojan
-wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
-unzip trojan-go-linux-amd64.zip
-
+mkdir
+trojan ~/trojan && cd ~/trojan
+if {find . -maxdepth 1 type f -name "trojan*" | grep -q .}; then
+  echo "trojan already exists"
+else
+  wget https://github.com/p4gefau1t/trojan-go/releases/download/v0.10.6/trojan-go-linux-amd64.zip
+  unzip trojan-go-linux-amd64.zip
+fi
+cp example/server.yaml .
 cp $certpath .
 cp $keypath .
